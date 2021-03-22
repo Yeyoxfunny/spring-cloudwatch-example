ad83af048e52dd47d269b0de050f79e33574e2c8
Sergio Morales <sergio.morales@valid.com>
2021-03-22 15:58:01 -0500

    Spring AWS CloudWatch examples

---
+++ aws-cloudwatch-microservice/src/main/java/co/com/smorales/aws/clouwatch/AwsClouwatchMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package co.com.smorales.aws.clouwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsClouwatchMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsClouwatchMicroserviceApplication.class, args);
+	}
+
+}
